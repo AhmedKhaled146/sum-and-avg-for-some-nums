e042e56c42aa283d561e0f8672921acca29fc61d
AhmedKhaled <a7med74yaso@gmail.com>
2022-07-18 17:40:25 +0200

    add program

---
+++ sum and avg of sum nums.py
@@ -0,0 +1,15 @@
+num_of_numbers = int(input("How many numbers you want to sum: "))
+
+def sum():
+    global sum 
+    sum = 0  
+    for x in range(num_of_numbers):  
+        sum += int(input(f"Enter Value {x + 1}: "))  
+
+    return sum  
+
+def avg():
+    return sum/num_of_numbers
+
+print("the sumation is", sum())
+print("the avgerage is", avg())
